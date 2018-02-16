a8b729a13eb37aabc9d36a858fcbe3ec59d2f8de
Igor Yatsevich <igor.nikolaevich.96@gmail.com>
2018-02-16 20:59:42 +0300

    Add new methods for DB works.

--- mainapp/src/main/java/com/epam/brest/cource/App.java
+++ mainapp/src/main/java/com/epam/brest/cource/App.java
@@ -1,15 +1,21 @@
 package com.epam.brest.cource;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws SQLException, ClassNotFoundException {
+
+        System.out.println( "Main module start." );
 
         DBUtils dbUtils = new DBUtils();
+        Connection connection = dbUtils.getConnection();
+        dbUtils.createUserTable(connection);
+        dbUtils.addUser(connection,"admin", "admin", "User admin");
+        dbUtils.addUser(connection,"admin1", "admin1", "User admin1");
+        dbUtils.addUser(connection,"admin2", "admin2", "User admin2");
+        dbUtils.getUser(connection);
+
     }
 }
