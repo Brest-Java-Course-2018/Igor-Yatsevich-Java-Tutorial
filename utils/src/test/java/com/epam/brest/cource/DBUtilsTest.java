00ec1d3561143af59a5a261019cecab271b6e75e
Igor Yatsevich <igor.nikolaevich.96@gmail.com>
2018-02-21 00:42:26 +0300

    Some fix for DELETE and INSERT tests.

--- utils/src/test/java/com/epam/brest/cource/DBUtilsTest.java
+++ utils/src/test/java/com/epam/brest/cource/DBUtilsTest.java
@@ -7,57 +7,58 @@ import java.sql.SQLException;
 
 public class DBUtilsTest {
 
-    @org.junit.Test
-    public void getConnection() throws SQLException, ClassNotFoundException {
+    private final DBUtils dbUtils = new DBUtils();
 
-        DBUtils dbUtils = new DBUtils();
+    @org.junit.Test
+    public void getConnection()
+            throws SQLException, ClassNotFoundException {
 
         Assert.assertNotNull(dbUtils.getConnection());
 
     }
 
     @org.junit.Test
-    public void deleteUser() throws SQLException, ClassNotFoundException {
-
-        DBUtils dbUtils = new DBUtils();
-
-        Connection connection = dbUtils.getConnection();
-        //dbUtils.createUserTable(connection);
-
-        dbUtils.addUser(connection,"admin", "admin", "User admin");
-        dbUtils.addUser(connection,"admin1", "admin1", "User admin1");
-        dbUtils.addUser(connection,"admin2", "admin2", "User admin2");
-
-        Assert.assertEquals(dbUtils.deleteUser(connection, 2), 1);
-        Assert.assertEquals(dbUtils.deleteUser(connection, 1), 1);
-        //Assert.assertEquals(dbUtils.deleteUser(connection, 4), 0);
-        Assert.assertEquals(dbUtils.deleteUser(connection, -5), 0);
-        Assert.assertEquals(dbUtils.deleteUser(connection, 0), 0);
-
-    }
-
-    @org.junit.Test
-    public void addUser() throws SQLException, ClassNotFoundException {
-
-        DBUtils dbUtils = new DBUtils();
+    public void addUser()
+            throws SQLException, ClassNotFoundException {
 
         Connection connection = dbUtils.getConnection();
+
         dbUtils.createUserTable(connection);
 
         Assert.assertEquals(dbUtils.addUser(connection,"admin", "admin", "User admin"), 1);
         Assert.assertEquals(dbUtils.addUser(connection,"admin1", "admin1", "User admin1"), 1);
         Assert.assertEquals(dbUtils.addUser(connection,"admin2", "admin2", "User admin2"), 1);
 
-        Assert.assertEquals(dbUtils.addUser(connection, null, "admin3", "User admin3"), -1);
-        Assert.assertEquals(dbUtils.addUser(connection, "admin3", null, "User admin3"), -1);
-        Assert.assertEquals(dbUtils.addUser(connection, "admin3", "admin3", null), -1);
-
         Assert.assertEquals(dbUtils.addUser(connection, "", "admin3", "User admin3"), -1);
         Assert.assertEquals(dbUtils.addUser(connection, "admin3", "", "User admin3"), -1);
         Assert.assertEquals(dbUtils.addUser(connection, "admin3", "admin3", ""), -1);
 
-        Assert.assertEquals(dbUtils.addUser(null, "admin3", "admin3", "User admin3"), -2);
+        Assert.assertEquals(dbUtils.addUser(connection, null, "admin3", "User admin3"), -2);
+        Assert.assertEquals(dbUtils.addUser(connection, "admin3", null, "User admin3"), -2);
+        Assert.assertEquals(dbUtils.addUser(connection, "admin3", "admin3", null), -2);
 
+        Assert.assertEquals(dbUtils.addUser(null, "admin3", "admin3", "User admin3"), -3);
 
     }
+
+    @org.junit.Test
+    public void deleteUser()
+            throws SQLException, ClassNotFoundException {
+
+        Connection connection = dbUtils.getConnection();
+
+        Assert.assertEquals(dbUtils.deleteUser(connection, 2), 1);
+        Assert.assertEquals(dbUtils.deleteUser(connection, 1), 1);
+
+        Assert.assertEquals(dbUtils.deleteUser(connection, 4), 0);
+
+        Assert.assertEquals(dbUtils.deleteUser(connection, -5), -1);
+        Assert.assertEquals(dbUtils.deleteUser(connection, 0), -1);
+
+        Assert.assertEquals(dbUtils.deleteUser(connection, null), -2);
+
+        Assert.assertEquals(dbUtils.deleteUser(null, null), -3);
+
+    }
+
 }
