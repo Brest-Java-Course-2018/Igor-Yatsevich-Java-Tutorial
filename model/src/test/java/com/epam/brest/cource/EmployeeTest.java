0bf82fbc7e25e3df6c9e60352625c3f604b1d915
Igor Yatsevich <igor.nikolaevich.96@gmail.com>
2018-02-26 20:01:28 +0300

    First app module.

---
+++ model/src/test/java/com/epam/brest/cource/EmployeeTest.java
@@ -0,0 +1,18 @@
+package com.epam.brest.cource;
+
+import org.junit.Assert;
+
+public class EmployeeTest {
+
+    private static final String ABDULA = "Abdula";
+
+    @org.junit.Test
+    public void getEmployeeName() {
+
+        Employee employee = new Employee();
+
+        employee.setEmployeeName(ABDULA);
+        Assert.assertEquals(ABDULA, employee.getEmployeeName());
+
+    }
+}
